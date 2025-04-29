2b233d7f2399fb7d80df378d457869f7a66605fd
Mariaapalacios <mp352@njit.edu>
2025-04-29 15:09:46 -0400

    status code print

--- web scraping/lec1.py
+++ web scraping/lec1.py
@@ -1,1 +1,10 @@
-print("hello world")
+import requests
+
+url="https://www.google.com"
+
+#request module, tenemos que hacer que sea stored en una var
+response=requests.get(url)
+print("Status code:",response.status_code)
+
+#if we wanted to do post-> submit
+#requests.post(url)
