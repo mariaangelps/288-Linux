ded5d1a8a77836ef9ad7a003a4bc3cc7b7f8d8f5
Mariaapalacios <mp352@njit.edu>
2025-05-11 18:46:04 -0400

    con el request a la pagina

--- web_scraping/www/3158891_Problem1.py
+++ web_scraping/www/3158891_Problem1.py
@@ -3,15 +3,24 @@ from pymongo import MongoClient
 from bs4 import BeautifulSoup
 import time
 
+client = MongoClient()
+db=client["stock_data"]
+collection = db["most_active_stocks"]
+
+
 url="https://finance.yahoo.com/most-active"
 response=requests.get(url)
 print("Status code:",response.status_code)
 
-#get data from html file
-with open("most_active.html", "r") as file:
-    html_file= file.read()
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+else:
 
-soup=BeautifulSoup(html_file,"html.parser")
+    #get data from html file
+    with open("most_active.html", "r") as file:
+        html_file= file.read()
+
+    soup=BeautifulSoup(html_file,"html.parser")
 
 #1. queremos encontrar primero las columnas symbol, name, price,change and volume
 #to find debugging steps
@@ -22,24 +31,35 @@ tag_th = t_head.find_all('th')
 
 #print each header
 for i in tag_th:
-    #strip to eliminate spaces
-    print(i.text.strip())
+    print(i.text.strip()) #strip to eliminate spaces
 
 #2. Extraer los datos de la tabla que estan dentro de tbody, obv busco primero la primera occurence de tbody
 t_body =soup.find('tbody')
-#encuentro tr que seria las rows
-t_tr=t_body.find_all('tr')
-index = 1  #esto para poder recorrer cada celda
+t_tr=t_body.find_all('tr') #encuentro tr que seria las rows
+stock_data=[]
 
 #hago un loop para iterar dentro de cada row, y sacar su columna
 for row in t_tr:
     #identifica cada celda como match de row y columna
-    celdas=i.find_all('td')
+    celdas=row.find_all('td')
     #extraer solo las 5 primeras
     if len(celdas) ==5:
-        data = [i.text.strip() for i in celdas]
-        print(f"{index} | {' | '.join(data)}")
-        index+=1
+        data = {
+                "symbol": celdas[0].text.strip(),
+                "name": celdas[1].text.strip(),
+                "price": celdas[2].text.strip(),
+                "change": celdas[3].text.strip(),
+                "volume": celdas[4].text.strip()
+            }
+        stock_data.append(data)
+    
+    if stock_data:
+        collection.insert_many(stock_data)
+        print(f"Inserted {len(stock_data)} new documents.")
+    
+    # Esperar 3 minutos antes de la siguiente actualización
+    print("Waiting for 3 minutes before the next update...")
+    time.sleep(180)  
 
 
 
@@ -47,7 +67,3 @@ for row in t_tr:
 
 
 
-
-#client = MongoClient()
-
-#db=client["soup"]
