a8b19133ea83e3e4bf20fdfbd6bd6d83bb80e61b
Mariaapalacios <mp352@njit.edu>
2025-05-11 18:17:59 -0400

    casi done

--- web_scraping/www/3158891_Problem1.py
+++ web_scraping/www/3158891_Problem1.py
@@ -1,6 +1,7 @@
 import requests
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+import time
 
 url="https://finance.yahoo.com/most-active"
 response=requests.get(url)
@@ -15,17 +16,35 @@ soup=BeautifulSoup(html_file,"html.parser")
 #1. queremos encontrar primero las columnas symbol, name, price,change and volume
 #to find debugging steps
 html_file.replace("\n","")
-#thead=soup.find('thead')
-tag_th = soup.find_all('th')
-t_body =soup.finall('tbody')
-#print(tag_th)
+#busco el 1er thead, despues me meto a ese y busco todos los headers de la tabla "th"
+t_head=soup.find('thead')
+tag_th = t_head.find_all('th')
+
+#print each header
 for i in tag_th:
     #strip to eliminate spaces
+    print(i.text.strip())
 
-    print(i.text,strip=True)
-for i in t_body:
-    #2. Extraer los datos de cada columna
-    print(i.contents)
+#2. Extraer los datos de la tabla que estan dentro de tbody, obv busco primero la primera occurence de tbody
+t_body =soup.find('tbody')
+#encuentro tr que seria las rows
+t_tr=t_body.find_all('tr')
+index = 1  #esto para poder recorrer cada celda
+
+#hago un loop para iterar dentro de cada row, y sacar su columna
+for row in t_tr:
+    #identifica cada celda como match de row y columna
+    celdas=i.find_all('td')
+    #extraer solo las 5 primeras
+    if len(celdas) ==5:
+        data = [i.text.strip() for i in celdas]
+        print(f"{index} | {' | '.join(data)}")
+        index+=1
+
+
+
+
+
 
 
 
